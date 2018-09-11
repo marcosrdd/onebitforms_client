a5a7022198b6f16e95085978f28343b7a0cce061
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-09-11 19:46:10 -0300

    intial setup and fixed dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 FROM node:9.2.0-slim
-ENV INSTALL_PATH /onbitforms-client
+ENV INSTALL_PATH /onbitformsclient
 RUN npm install -g @angular/cli
 RUN mkdir -p $INSTALL_PATH
 WORKDIR $INSTALL_PATH
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import { MaterializeModule } from 'ng2-materialize';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterializeModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
