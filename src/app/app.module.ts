f06fb8ecb3d1eaed2f46f15ebe2afc7246074e89
Marcos Robson Dias Duarte <marcosrdd@gmail.com>
2018-09-12 12:27:02 -0300

    updated app module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+ 
 import { AppComponent } from './app.component';
-
 import { MaterializeModule } from 'ng2-materialize';
 import { ColorPickerModule } from 'ngx-color-picker';
 import { ChartsModule } from 'ng2-charts';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { NotFoundComponent } from './not-found/not-found.component';
-
+import { FormsModule } from './forms/forms.module';
+import { AnswersModule } from './answers/answers.module';
+ 
+ 
 @NgModule({
   declarations: [
     AppComponent,
@@ -21,7 +23,9 @@ import { NotFoundComponent } from './not-found/not-found.component';
     BrowserModule,
     MaterializeModule.forRoot(),
     ColorPickerModule,
-    ChartsModule
+    ChartsModule,
+    FormsModule,
+    AnswersModule
   ],
   providers: [],
   bootstrap: [AppComponent]
